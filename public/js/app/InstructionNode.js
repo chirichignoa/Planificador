c84b64e81c608f6beeafce34666df25ba02fadc6
chiri <chirichignoa@gmail.com>
2017-02-14 20:20:07 -0300

    Agregado de variable que indica si pertenece al camino critico

--- public/js/app/InstructionNode.js
+++ public/js/app/InstructionNode.js
@@ -6,6 +6,7 @@ define(function () {
         this.latenciaAcum = latenciaAcum;
         this.dependencias = [];
         this.dependientes = [];
+        this.caminoCritico = false;
     }
 
     InstructionNode.prototype = (function () {

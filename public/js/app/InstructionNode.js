40403803d740f17d6da836775d7468f17849835a
chiri <chirichignoa@gmail.com>
2017-02-14 21:21:11 -0300

    Agregado de metodos

--- public/js/app/InstructionNode.js
+++ public/js/app/InstructionNode.js
@@ -13,6 +13,48 @@ define(function () {
 
         return {
             constructor: InstructionNode,
+
+        getInstr: function() {
+            return this.instr;
+        },
+
+        getAcumLatency: function(){
+            return this.acumLatency;
+        }, 
+        
+        getDependencies: function(){
+            return this.dependencies;
+        },
+
+        getDependents: function(){
+            return this.dependents;
+        },
+
+        getCriticalPath: function(){
+            return this.criticalPath;
+        },
+
+        setDependencies: function(dependencies){
+            this.dependencies = dependencies;
+        },
+
+        setDependents: function(dependents){
+            this.dependents = dependents;
+        },
+
+        setCriticalPath: function(){
+            this.criticalPath = true;
+        },
+
+        printInstructionNode : function() {
+            console.log("Nodo: ");
+            console.log("Instr "+ this.instr);
+            console.log("AcumLatency: "+ this.acumLatency);
+            console.log("Dependencies: "+ this.dependencies.toString());
+            console.log("Dependents: "+ this.dependents.toString());
+            console.log("CriticalPath: "+ this.criticalPath);
+        },
+
         }
 
     }

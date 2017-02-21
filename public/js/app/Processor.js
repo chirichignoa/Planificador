819b151b37794ef664912d8960fc89df1e349eac
chiri <chirichignoa@gmail.com>
2017-02-21 16:16:22 -0300

    Correccion error
    Faltaba llamar al toString()

--- public/js/app/Processor.js
+++ public/js/app/Processor.js
@@ -186,7 +186,7 @@ define(["Instruction","InstructionNode", "FunctionalUnit"], function (Instructio
             nextCycle: function () {
                 if(this.criticalPath.length > 0) {
                     var instrCritical = this.criticalPath[0];
-                    console.log("instrCritical: "+instrCritical.);
+                    console.log("instrCritical: "+instrCritical.toString());
                     var fu = this.availableUF(instrCritical.getInstr().getType());
                     if(this.canRun(instrCritical)) { //Se puede ejecutar 
                         if(fu != -1) { //Hay UF disponibles

d2f960778aada5bf76795a037f99a65da05168f9
Damian Dominguez <damian93domin@gmail.com>
2017-02-21 17:33:13 -0300

    Correcion &&

--- public/js/app/Processor.js
+++ public/js/app/Processor.js
@@ -159,7 +159,7 @@ define(["Instruction","InstructionNode", "FunctionalUnit"], function (Instructio
 
             run: function () {
                 var finished = false;
-                while (this.planned.length != 0) && (!finished) {
+                while ((this.planned.length != 0) && (!finished)) {
                     console.log("Ciclo numero: "+ this.currentCycle);
                     this.nextCycle();
                     this.currentCycle += 1;                        
